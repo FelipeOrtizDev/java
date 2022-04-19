c0a44ae996869cc84d86bcee218bedb6e313d591
FelipeOrtiz-Dev <103068468+FelipeOrtiz-Dev@users.noreply.github.com>
2022-04-19 09:25:55 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { Pet } from "./petshopModulo";
+
+let pet = new Pet("Rex", 5, 4.5);
---
+++ src/petshopModulo.js
@@ -0,0 +1,5 @@
+export function Pet(nomeDoPet, idade, peso) {
+  this.nome = nomeDoPet;
+  this.idade = idade;
+  this.peso = peso;
+}
